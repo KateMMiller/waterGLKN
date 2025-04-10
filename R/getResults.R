09f8ad56ef0874f884e3288b58a0a639453e6904
KateMMiller <kathryn_miller@nps.gov>
2025-04-10 16:19:28 -0400

    code cleanup

--- R/getResults.R
+++ R/getResults.R
@@ -4,7 +4,7 @@
 #'
 #' @description Queries GLKN water data by site, year, month, parameter, etc.
 #'
-#' @importFrom dplyr filter
+#' @importFrom dplyr filter full_join left_join
 #'
 #' @param park Combine data from all parks or one or more parks at a time. Valid inputs:
 #'  \describe{
@@ -57,7 +57,7 @@
 #' @param years Numeric. Years to query. Accepted values start at 2007.
 #'
 #' @param months Numeric. Months to query by number. Accepted values range from 1:12. Default month range is
-#' 4:10, as most non-QAQC sample events occur between the months of April and October.
+#' 4:11, as most non-QAQC sample events occur between the months of April and November.
 #'
 #' @param active Logical. If TRUE (Default), only returns actively monitored locations. If FALSE, returns all
 #' locations that have been monitored at least once since 2007. Active sites are defined as sites that have at
@@ -99,7 +99,7 @@
 getResults <- function(park = "all", site = "all", site_type = "all",
                        sample_type = "VS",
                        years = 2007:format(Sys.Date(), "%Y"),
-                       months = 4:10,
+                       months = 4:11,
                        active = TRUE,
                        parameter = "all",
                        sample_depth = 'all',
@@ -187,27 +187,23 @@ getResults <- function(park = "all", site = "all", site_type = "all",
     res1$Lower_Quantification_Limit[res1$Result_Detection_Condition %in% "Present Below Quantification Limit"]
   res1$censored <- ifelse(res1$Result_Detection_Condition %in% "Detected and Quantified", FALSE, TRUE)
 
-  res2 <- if(any(park %in% "all")){res1
-  } else {filter(res1, Park_Code %in% park)}
+  res2 <- if(any(parameter %in% "all")){res1
+  } else {filter(res1, param_name %in% parameter)}
+  res3 <- filter(res2, year %in% years)
+  res4 <- filter(res3, month %in% months)
+  res5 <- if(any(sample_type %in% "all")){res4
+  } else {filter(res4, samp_type %in% sample_type)}
 
-  res3 <- if(any(parameter %in% "all")){res2
-  } else {filter(res2, param_name %in% parameter)}
+  res6 <- if(any(sample_depth %in% "all")){res5
+  } else {filter(res5, Activity_Relative_Depth %in% "surface")}
 
-  res4 <- filter(res3, year %in% years)
-  res5 <- filter(res4, month %in% months)
-  res6 <- if(any(sample_type %in% "all")){res5
-  } else {filter(res5, samp_type %in% sample_type)}
+  res7 <- if(any(include_censored == TRUE)){res6
+  } else {filter(res6, censored == FALSE)}
 
-  res7 <- if(any(sample_depth %in% "all")){res6
-  } else {filter(res6, Activity_Relative_Depth %in% "surface")}
+  res8 <- full_join(loc, res7, by = c("Org_Code", "Location_ID", "Park_Code"))
 
-  res8 <- if(any(include_censored == TRUE)){res7
-  } else {filter(res7, censored == FALSE)}
-
-  res9 <- left_join(loc, res8, by = c("Org_Code", "Location_ID", "Park_Code"))
-
-  res_final <- if(any(output == "verbose")){res9
-  } else {res9[,c("Org_Code","Park_Code", "Project_ID", "Location_ID", "Location_Name", "site_type", "protocol",
+  res_final <- if(any(output == "verbose")){res8
+  } else {res8[,c("Org_Code","Park_Code", "Project_ID", "Location_ID", "Location_Name", "site_type", "protocol",
                   "sample_date", "year", "month", "doy",
                   "Activity_Relative_Depth", "Activity_Depth", "Activity_Depth_Unit", "Activity_Group_Type", "samp_type",
                   "Characteristic_Name", "param_name", "Result_Detection_Condition",
